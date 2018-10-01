6d9a48b178d40e1d96a1ac4add66a33d1f987f7a
sxt139956 <42833943+sxt139956@users.noreply.github.com>
2018-10-01 10:17:16 +0000

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBy2VkQXTgCWkfWfgejVUcKH5GuO2DFAds');
+define('APIKEY', 'AIzaSyAurg5k-lp9qQxA_IpC5x48YZ3-cslEQ5Q');
 define('GJ_CODE', 'TW');
 define('SITE_NAME', 'HELLO WORLD 你好 世界');
-define('TITLENAME', '宋老师儿Tube-台湾');
+define('TITLENAME', '宋老师Tube-台湾');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
 ?>
